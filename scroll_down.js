cacae2bce00bdf42f35bba9353cbdbf0ce85a4b0
chaos <chaos29092@gmail.com>
2015-12-26 14:52:41 +0800

    add scroll down

---
+++ scroll_down.js
@@ -0,0 +1,8 @@
+window.onload=scroll_down();
+
+function scroll_down() {
+    for (var i = 0; i < 100; i++) {
+        setTimeout("scroll(0,100);", 3000 * i + 1000);
+        setTimeout("scroll(0,document.body.scrollHeight)", 3000 * i);
+    }
+}
